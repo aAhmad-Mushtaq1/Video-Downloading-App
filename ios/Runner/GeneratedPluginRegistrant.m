4db00d987113588f9551a501d4a97b5d943cdce2
Muhammad Ahmad Mushtaq <24pwswe0018@uetpeshawar.edu.pk>
2026-05-09 16:14:24 +0500

    Recovered code from local backup

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,42 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<permission_handler_apple/PermissionHandlerPlugin.h>)
+#import <permission_handler_apple/PermissionHandlerPlugin.h>
+#else
+@import permission_handler_apple;
+#endif
+
+#if __has_include(<share_plus/FPPSharePlusPlugin.h>)
+#import <share_plus/FPPSharePlusPlugin.h>
+#else
+@import share_plus;
+#endif
+
+#if __has_include(<sqflite_darwin/SqflitePlugin.h>)
+#import <sqflite_darwin/SqflitePlugin.h>
+#else
+@import sqflite_darwin;
+#endif
+
+#if __has_include(<url_launcher_ios/URLLauncherPlugin.h>)
+#import <url_launcher_ios/URLLauncherPlugin.h>
+#else
+@import url_launcher_ios;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+  [FPPSharePlusPlugin registerWithRegistrar:[registry registrarForPlugin:@"FPPSharePlusPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+  [URLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"URLLauncherPlugin"]];
+}
+
+@end
--- windows/runner/utils.cpp
+++ windows/runner/utils.cpp
@@ -1,55 +1,65 @@
 #include "utils.h"
 
+#include <flutter_windows.h>
+#include <io.h>
+#include <stdio.h>
 #include <windows.h>
-#include <codecvt>
+
 #include <iostream>
-#include <locale>
 
 void CreateAndAttachConsole() {
   if (::AllocConsole()) {
-    FILE* unused;
-    freopen_s(&unused, "CONOUT$", "w", stdout);
-    freopen_s(&unused, "CONOUT$", "w", stderr);
+    FILE *unused;
+    if (freopen_s(&unused, "CONOUT$", "w", stdout)) {
+      _dup2(_fileno(stdout), 1);
+    }
+    if (freopen_s(&unused, "CONOUT$", "w", stderr)) {
+      _dup2(_fileno(stdout), 2);
+    }
+    std::ios::sync_with_stdio();
+    FlutterDesktopResyncOutputStreams();
   }
 }
 
 std::vector<std::string> GetCommandLineArguments() {
-  std::vector<std::string> arguments;
-
+  // Convert the UTF-16 command line arguments to UTF-8 for the Engine to use.
   int argc;
   wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
   if (argv == nullptr) {
-    return arguments;
+    return std::vector<std::string>();
   }
 
-  std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> wide_to_utf8;
-  for (int i = 1; i < argc; ++i) {
-    arguments.push_back(wide_to_utf8.to_bytes(argv[i]));
+  std::vector<std::string> command_line_arguments;
+
+  // Skip the first argument as it's the binary name.
+  for (int i = 1; i < argc; i++) {
+    command_line_arguments.push_back(Utf8FromUtf16(argv[i]));
   }
 
   ::LocalFree(argv);
 
-  return arguments;
+  return command_line_arguments;
 }
 
-std::wstring Utf16FromUtf8(const std::string& utf8_string) {
-  if (utf8_string.empty()) {
-    return std::wstring();
+std::string Utf8FromUtf16(const wchar_t* utf16_string) {
+  if (utf16_string == nullptr) {
+    return std::string();
   }
-  int target_length =
-      ::MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, utf8_string.data(),
-                            static_cast<int>(utf8_string.length()), nullptr, 0);
-  if (target_length == 0) {
-    return std::wstring();
+  unsigned int target_length = ::WideCharToMultiByte(
+      CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
+      -1, nullptr, 0, nullptr, nullptr)
+    -1; // remove the trailing null character
+  int input_length = (int)wcslen(utf16_string);
+  std::string utf8_string;
+  if (target_length == 0 || target_length > utf8_string.max_size()) {
+    return utf8_string;
   }
-  std::wstring utf16_string;
-  utf16_string.resize(target_length);
-  int converted_length =
-      ::MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, utf8_string.data(),
-                            static_cast<int>(utf8_string.length()),
-                            utf16_string.data(), target_length);
+  utf8_string.resize(target_length);
+  int converted_length = ::WideCharToMultiByte(
+      CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
+      input_length, utf8_string.data(), target_length, nullptr, nullptr);
   if (converted_length == 0) {
-    return std::wstring();
+    return std::string();
   }
-  return utf16_string;
+  return utf8_string;
 }
